20c2d5cdcb2209cfcf898721b673db97bbcc36b4
lokideos <archmage09@gmail.com>
2021-07-27 19:47:18 +0300

    gs

--- lib/safelylaunch.rb
+++ lib/safelylaunch.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 require 'safelylaunch/version'
+require 'safelylaunch/client'
+require 'safelylaunch/http_connection'
+require 'safelylaunch/mock_connection'
 
 module Safelylaunch
   class Error < StandardError; end

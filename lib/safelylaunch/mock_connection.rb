66437c21908fd4ce7378cceb10d457379017ea62
lokideos <archmage09@gmail.com>
2021-07-28 12:40:54 +0300

    Fix tests; add toggles to mock connetion

--- lib/safelylaunch/mock_connection.rb
+++ lib/safelylaunch/mock_connection.rb
@@ -2,15 +2,20 @@ module Safelylaunch
   class MockConnection
     attr_reader :api_token, :logger, :host, :connection
 
-    def initialize(api_token:, logger: Logger.new(STDOUT), host: 'http://localhost:2300')
+    def initialize(api_token:, toggles: {}, logger: Logger.new(STDOUT), host: 'http://localhost:2300')
       @api_token = api_token
       @logger = logger
       @host = host
+      @toggles = toggles
       @connection = nil
     end
 
-    def get(_key)
-      { "key": "stream-toggle", "enable": false }
+    def get(key)
+      result = @toggles.fetch(key,  false )
+      {
+        key: key,
+        enable: result
+      }
     end
   end
 end
--- spec/unit/mock_connection_spec.rb
+++ spec/unit/mock_connection_spec.rb
@@ -1,11 +1,24 @@
 # frozen_string_literal: true
 
 RSpec.describe Safelylaunch::MockConnection do
-  subject(:conn) { described_class.new(api_token: api_token) }
+  subject(:conn) { described_class.new(api_token: api_token, toggles: toggles) }
 
   let(:api_token) { '123' }
+  let(:toggles) do
+    {
+      'toggle-key' => true,
+      'disable-toggle-key' => false,
+    }
+  end
 
   describe '#call' do
-    it { expect(conn.get('anything')).to eq("key": "stream-toggle", "enable": false)  }
+    context 'when mock connection knows about toggle key' do
+      it { expect(conn.get('toggle-key')).to eq("key": "toggle-key", "enable": true) }
+      it { expect(conn.get('disable-toggle-key')).to eq("key": "disable-toggle-key", "enable": false) }
+    end
+
+    context 'when mock connection does not know about toggle key' do
+      it { expect(conn.get('anything-else')).to eq("key": 'anything-else', "enable": false) }
+    end
   end
 end
